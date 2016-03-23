e025ef267edf226c80917bd7ee7afbb71ebe8845
JohnnyDeeee <gamefake@live.nl>
2016-03-23 13:48:05 +0100

    Added Hints to the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class GameManager
+{
+    public static GameObject pr_hint;
+    private static bool initialized = false;
+    private static Interface gui;
+
+    //One-time initialization
+    public static void Initialize()
+    {
+        if (!initialized)
+        {
+            //Load resources
+            pr_hint = Resources.Load<GameObject>("Prefabs/GUI/Hint");
+
+            //Get the interface script
+            gui = GameObject.Find("GUI").GetComponent<Interface>();
+
+            //End of initialization
+            initialized = true; 
+        }
+    }
+}
---
+++ Assets/Scripts/Interface/Interface.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Interface : MonoBehaviour
+{
+
+    void Awake()
+    {
+        GameManager.Initialize();
+    }
+
+	void Start ()
+    {
+        StartCoroutine(temp());
+	}
+
+    private IEnumerator temp()
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        ShowHint("Test", "GameManager is initialized :)", 3.0f);
+    }
+
+    //Show a hint on screen for x seconds
+    public void ShowHint(string title, string message, float seconds)
+    {
+        GameObject hint = Object.Instantiate(GameManager.pr_hint);
+        hint.transform.SetParent(gameObject.transform);
+        hint.transform.localPosition = new Vector2(0, 0);
+        hint.transform.FindChild("Title").GetComponent<Text>().text = title;
+        hint.transform.FindChild("Message").GetComponent<Text>().text = message;
+        StartCoroutine(HintTimer(hint, seconds));
+    }
+
+    public IEnumerator HintTimer(GameObject hint, float seconds)
+    {
+        hint.SetActive(true);
+
+        yield return new WaitForSeconds(seconds);
+
+        hint.SetActive(false);
+        Destroy(hint);
+    }
+}
